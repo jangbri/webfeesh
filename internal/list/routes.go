c3ed37c5ca84dba481a02fe7d442bf94b889e981
jangbri <jangbri@gmail.com>
2026-05-27 15:50:19 -0400

    fix: follow restful resource naming convention

--- internal/list/routes.go
+++ internal/list/routes.go
@@ -3,11 +3,11 @@ package list
 import "net/http"
 
 func RegisterRoutes(mux *http.ServeMux, h *Handler) {
-	mux.HandleFunc("GET /list/{$}", h.GetAll)
-	mux.HandleFunc("GET /list/{id}", h.GetListFeeds)
+	mux.HandleFunc("GET /lists/{$}", h.GetAll)
+	mux.HandleFunc("GET /lists/{id}", h.GetListFeeds)
 
-	mux.HandleFunc("POST /list/{$}", h.Create)
-	mux.HandleFunc("POST /list/{id}", h.Update)
+	mux.HandleFunc("POST /lists/{$}", h.Create)
+	mux.HandleFunc("POST /lists/{id}", h.Update)
 
-	mux.HandleFunc("DELETE /list/{id}", h.Delete)
+	mux.HandleFunc("DELETE /lists/{id}", h.Delete)
 }
