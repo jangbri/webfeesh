8b8e366277761188f9800ffe88f095a89b8e3232
jangbri <jangbri@gmail.com>
2026-06-11 17:25:32 -0400

    fix: when frontend is built, displayed url fix
    The <pre> tags that show the backend api endpoint that returns the rss will now reflect the correct origin, regardless of where it is hosted.

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const isProd = import.meta.env.PROD
 
-export const apiURL = isProd ? '/api' : import.meta.env.VITE_BACKEND_BASE_URL
+export const apiURL = isProd ? window.location.origin : import.meta.env.VITE_BACKEND_BASE_URL
 
 export const api = axios.create({
   baseURL: apiURL,
